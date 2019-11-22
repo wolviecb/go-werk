2f69c15a6de5106dbb6be412f9e03fac826ce0de
Tom Andrade <wolvie@gmail.com>
2019-11-22 09:35:58 +0100

    move from individual flags to directly populating the cfg struct

--- go-werk.go
+++ go-werk.go
@@ -16,46 +16,32 @@ import (
 
 const appVersion = "0.1"
 
-// default that can be overridden from the command line
-var versionFlag bool
-var allowRedirectsFlag bool
-var helpFlag bool
-var disableCompression bool
-var disableKeepAlive bool
-var goroutines int
-var duration int
-var timeoutms int
-var method string
-var host string
-var headerStr string
-var reqBody string
-var clientCert string
-var clientKey string
-var caCert string
-var http2 bool
-var insecureTLS bool
-var testURL string
-var header map[string]string
-var statsAggregator chan *loader.RequesterStats
+var (
+	f loader.LoadCfg
+
+	helpFlag    bool
+	headerStr   string
+	versionFlag bool
+)
 
 func init() {
 	flag.BoolVar(&versionFlag, "v", false, "Print version details")
-	flag.BoolVar(&allowRedirectsFlag, "redir", false, "Allow Redirects")
+	flag.BoolVar(&f.AllowRedirects, "redir", false, "Allow Redirects")
 	flag.BoolVar(&helpFlag, "help", false, "Print help")
-	flag.BoolVar(&disableCompression, "no-c", false, "Disable Compression - Prevents sending the \"Accept-Encoding: gzip\" header")
-	flag.BoolVar(&disableKeepAlive, "no-ka", false, "Disable KeepAlive - prevents re-use of TCP connections between different HTTP requests")
-	flag.IntVar(&goroutines, "c", 10, "Number of goroutines to use (concurrent connections)")
-	flag.IntVar(&duration, "d", 10, "Duration of test in seconds")
-	flag.IntVar(&timeoutms, "T", 1000, "Socket/request timeout in ms")
-	flag.StringVar(&method, "M", "GET", "HTTP method")
-	flag.StringVar(&host, "host", "", "Host Header")
+	flag.BoolVar(&f.DisableCompression, "no-c", false, "Disable Compression - Prevents sending the \"Accept-Encoding: gzip\" header")
+	flag.BoolVar(&f.DisableKeepAlive, "no-ka", false, "Disable KeepAlive - prevents re-use of TCP connections between different HTTP requests")
+	flag.IntVar(&f.Goroutines, "c", 10, "Number of goroutines to use (concurrent connections)")
+	flag.IntVar(&f.Duration, "d", 10, "Duration of test in seconds")
+	flag.IntVar(&f.Timeoutms, "T", 1000, "Socket/request timeout in ms")
+	flag.StringVar(&f.Method, "M", "GET", "HTTP method")
+	flag.StringVar(&f.Host, "host", "", "Host Header")
 	flag.StringVar(&headerStr, "H", "", "header line, joined with ';'")
-	flag.StringVar(&reqBody, "body", "", "request body string or @filename")
-	flag.StringVar(&clientCert, "cert", "", "CA certificate file to verify peer against (SSL/TLS)")
-	flag.StringVar(&clientKey, "key", "", "Private key file name (SSL/TLS")
-	flag.StringVar(&caCert, "ca", "", "CA file to verify peer against (SSL/TLS)")
-	flag.BoolVar(&http2, "http", true, "Use HTTP/2")
-	flag.BoolVar(&insecureTLS, "insecure", true, "verify TLS certificates")
+	flag.StringVar(&f.ReqBody, "body", "", "request body string or @filename")
+	flag.StringVar(&f.ClientCert, "cert", "", "CA certificate file to verify peer against (SSL/TLS)")
+	flag.StringVar(&f.ClientKey, "key", "", "Private key file name (SSL/TLS")
+	flag.StringVar(&f.CaCert, "ca", "", "CA file to verify peer against (SSL/TLS)")
+	flag.BoolVar(&f.HTTP2, "http", true, "Use HTTP/2")
+	flag.BoolVar(&f.InsecureTLS, "insecure", true, "toggle verify TLS certificates")
 }
 
 // printDefaults a nicer format for the defaults
@@ -70,64 +56,61 @@ func printDefaults() {
 
 func main() {
 	// raising the limits. Some performance gains were achieved with the + goroutines (not a lot).
-	runtime.GOMAXPROCS(runtime.NumCPU() + goroutines)
+	runtime.GOMAXPROCS(runtime.NumCPU() + f.Goroutines)
 
-	statsAggregator = make(chan *loader.RequesterStats, goroutines)
+	f.StatsAggregator = make(chan *loader.RequesterStats, f.Goroutines)
 	sigChan := make(chan os.Signal, 1)
 
 	signal.Notify(sigChan, os.Interrupt)
 
 	flag.Parse() // Scan the arguments list
-	header = make(map[string]string)
+	f.Header = make(map[string]string)
 	if headerStr != "" {
 		headerPairs := strings.Split(headerStr, ";")
 		for _, hdr := range headerPairs {
 			hp := strings.SplitN(hdr, ":", 2)
-			header[hp[0]] = hp[1]
+			f.Header[hp[0]] = hp[1]
 		}
 	}
 
 	if na := flag.Args(); len(na) < 1 {
 		printDefaults()
 	} else {
-		testURL = flag.Args()[0]
+		f.TestURL = flag.Args()[0]
 	}
 
 	if versionFlag {
 		fmt.Println("Version:", appVersion)
 		return
-	} else if helpFlag || len(testURL) == 0 {
+	} else if helpFlag || len(f.TestURL) == 0 {
 		printDefaults()
 	}
 
-	fmt.Printf("Running %vs test @ %v\n  %v goroutine(s) running concurrently\n", duration, testURL, goroutines)
+	fmt.Printf("Running %vs test @ %v\n  %v goroutine(s) running concurrently\n", f.Duration, f.TestURL, f.Goroutines)
 
-	if len(reqBody) > 0 && reqBody[0] == '@' {
-		bodyFilename := reqBody[1:]
+	if len(f.ReqBody) > 0 && f.ReqBody[0] == '@' {
+		bodyFilename := f.ReqBody[1:]
 		data, err := ioutil.ReadFile(bodyFilename)
 		if err != nil {
 			fmt.Println(fmt.Errorf("could not read file %q: %v", bodyFilename, err))
 			os.Exit(1)
 		}
-		reqBody = string(data)
+		f.ReqBody = string(data)
 	}
 
-	loadGen := loader.NewLoadCfg(duration, goroutines, testURL, reqBody, method, host, header, statsAggregator, timeoutms,
-		allowRedirectsFlag, disableCompression, disableKeepAlive, clientCert, clientKey, caCert, http2, insecureTLS)
-
-	for i := 0; i < goroutines; i++ {
-		go loadGen.RunSingleLoadSession()
+	for i := 0; i < f.Goroutines; i++ {
+		go f.RunSingleLoadSession()
 	}
 
 	responders := 0
 	aggStats := loader.RequesterStats{MinRequestTime: time.Minute}
 
-	for responders < goroutines {
+	for responders < f.Goroutines {
 		select {
 		case <-sigChan:
-			loadGen.Stop()
+			f.Stop()
 			fmt.Printf("stopping...\n")
-		case stats := <-statsAggregator:
+		case stats := <-f.StatsAggregator:
 			aggStats.NumErrs += stats.NumErrs
 			aggStats.NumRequests += stats.NumRequests
 			aggStats.TotRespSize += stats.TotRespSize
